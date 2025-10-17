bb4e564b3f3290861f30d4f03f0832b8fc198daa
Nyala Jackson <nymjackson@gmail.com>
2025-10-17 12:02:01 -0700

    Added db

--- db.js
+++ db.js
@@ -1,4 +1,17 @@
 import {MongoClient} from 'mongodb'
 const connStr = process.env.ATLAS_URI
 
-const connection = new MongoClient(connStr)
+const client = new MongoClient(connStr)
+
+let conn;
+try{
+conn = await client.connect()
+console.log("Connection with MongoDB Established.")
+} catch(e){
+console.log(e)
+}
+const dbName = ""
+const db = connection.db(dbName)
+console.log("Conneced to ", dbName)
+
+export default db;
