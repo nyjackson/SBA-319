2a0a3fb58bca0baefb8c9195f1ec90f8d45749fd
Nyala Jackson <nymjackson@gmail.com>
2025-10-20 16:33:07 -0400

    Added validator and working on addClass on strength route

--- controllers/strengthController.js
+++ controllers/strengthController.js
@@ -1,31 +1,81 @@
-import data from '../data/strength.js'
-import db from '../db.js'
+import data from "../data/strength.js";
+import db from "../db.js";
+import { ObjectId } from "mongodb";
 
-async function getClasses(req,res){
-        let strength_classes;
-        console.log("In Get Strength Classes")
-        try{
-            const collection = await db.collection("strength")
-            strength_classes = await collection.find({}).toArray()
-        }
-        catch(e){
-            console.log(e)
-        }
-        res.status(200).json(strength_classes)
+async function getClasses(req, res) {
+  let strength_classes;
+  console.log("In Get Strength Classes");
+  try {
+    const collection = await db.collection("strength");
+    strength_classes = await collection.find({}).toArray();
+  } catch (e) {
+    console.log(e);
+  }
+  res.status(200).json(strength_classes);
+}
+
+async function resetClasses(req, res) {
+  try {
+    const collection = await db.collection("strength");
+    let clear = await collection.deleteMany({});
+    let resetResult = await collection.insertMany(data);
+    res
+      .status(200)
+      .json({
+        message: "Strength classes reset to the original 10 classes.",
+        result: resetResult,
+        defaultData: data,
+      });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function addClass(req, res) {
+  try {
+    const collection = await db.collection("strength");
+    const validator = {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "class validator",
+        required: ["_id", "name", "sub_type", "type", "weapon"],
+        properties: {
+          _id: {
+            bsonType: "objectId",
+          },
+          name: {
+            bsonType: "string",
+          },
+          sub_type: {
+            bsonType: "string",
+          },
+          type: {
+            enum: ["magic", "strength", "endurance"]
+          },
+          weapon: {
+            bsonType: "string",
+          },
+        },
+      },
     }
-async function resetClasses(req,res){
-    try{
-        const collection = await db.collection("strength")
-        let clear = await collection.deleteMany({})
-        let resetResult = await collection.insertMany(data)
-        res.status(200).json({message: "Strength classes reset to the original 10 classes.", result: resetResult, defaultData: data})
-    }
-    catch(e){
-        console.log(e)
-    }
+    const applyRules = await db.command({collMod: "strength", validator})
+    console.log(applyRules)
+    let result = await collection.insertOne(req.body)
+    res
+      .status(200)
+      .json({
+        message: "New Strength Class Added",
+        addedClass: result,
+      });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default {
-    get: getClasses,
-    default: resetClasses
-}
+  get: getClasses,
+  default: resetClasses,
+  add: addClass,
+ // edit: editClass,
+ // remove: removeClass,
+};
--- routes/classStrength.js
+++ routes/classStrength.js
@@ -5,10 +5,12 @@ const router = express.Router()
 
 router.get("/", strengthController.get)
 
+router.post("/add", strengthController.add)
+// router.delete("/remove", strengthController.removeClass)
+// router.put("/edit", strengthController.editClass)
 
 router.route('/reset')
 .post(strengthController.default)
-
 
 
 export default router
